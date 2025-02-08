2b7c4c7568a3b7e8abac046294115600cb80832e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-08 14:16:34 +0000

    Reset to 11ccfe4ec5dc4e10465cdca927d5839fc1a1a3d7 and made changes for LLM Round 1

--- strategies/constant_percentage_strategy.py
+++ strategies/constant_percentage_strategy.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from strategies.base_strategy import BaseStrategy
+from database.models import Balance
 
 class ConstantPercentageStrategy(BaseStrategy):
     def __init__(self, broker, stock_allocations, cash_percentage, rebalance_interval_minutes, starting_capital):
@@ -14,30 +15,33 @@ class ConstantPercentageStrategy(BaseStrategy):
     def rebalance(self):
         account_info = self.broker.get_account_info()
         cash_balance = account_info.get('cash_available')
-        total_balance = account_info.get('total_balance')
+        with self.broker.Session() as session:
+            balance = session.query(Balance).filter_by(
+                strategy=self.strategy_name,
+                broker=self.broker.broker_name,
+                type='cash'  # Added filter for balance type
+            ).first()
+            if balance is None:
+                raise ValueError(f'Strategy balance not initialized for {self.strategy_name} strategy on {self.broker}.')
+
+            total_balance = balance.total_balance  # Derive total balance from the queried balance object
 
         target_cash_balance = total_balance * self.cash_percentage
         target_investment_balance = total_balance - target_cash_balance
 
         current_positions = self.get_current_positions()
 
-        with self.broker.Session() as session:
-            balance = session.query(Balance).filter_by(
-                strategy=self.strategy_name,
-                broker=self.broker.broker_name
-            ).first()
-            if balance is None:
-                raise ValueError(f'Strategy balance not initialized for {self.strategy_name} strategy on {self.broker.broker_name}.')
+        # TODO: Query the number of current positions in the DB for each ticker associated with this strategy, and then get their current value.
 
-            for stock, allocation in self.stock_allocations.items():
-                target_balance = target_investment_balance * allocation
-                current_position = current_positions.get(stock, 0)
-                current_price = self.broker.get_current_price(stock)
-                target_quantity = target_balance // current_price
-                if current_position < target_quantity:
-                    self.broker.place_order(stock, target_quantity - current_position, 'buy', self.strategy_name)
-                elif current_position > target_quantity:
-                    self.broker.place_order(stock, current_position - target_quantity, 'sell', self.strategy_name)
+        for stock, allocation in self.stock_allocations.items():
+            target_balance = target_investment_balance * allocation
+            current_position = current_positions.get(stock, 0)
+            current_price = self.broker.get_current_price(stock)
+            target_quantity = target_balance // current_price
+            if current_position < target_quantity:
+                self.broker.place_order(stock, target_quantity - current_position, 'buy', self.strategy_name)
+            elif current_position > target_quantity:
+                self.broker.place_order(stock, current_position - target_quantity, 'sell', self.strategy_name)
 
     def get_current_positions(self):
         positions = self.broker.get_positions()
