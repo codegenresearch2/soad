2d891fc13650f8f27245fbde8ab96f8f9b1c6832
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:12:24 +0000

    Reset to 11ccfe4ec5dc4e10465cdca927d5839fc1a1a3d7 and made changes for LLM Round 1

--- strategies/base_strategy.py
+++ strategies/base_strategy.py
@@ -12,24 +12,23 @@ class BaseStrategy(ABC):
 
     def initialize_starting_balance(self):
         account_info = self.broker.get_account_info()
-        buying_power = account_info.get('buying_power')
+        cash_balance = account_info.get('cash_available')
 
-        if buying_power < self.starting_capital:
+        if cash_balance < self.starting_capital:
             raise ValueError("Not enough cash available to initialize the strategy with the desired starting capital.")
 
         with self.broker.Session() as session:
             strategy_balance = session.query(Balance).filter_by(
                 strategy=self.strategy_name,
-                broker=self.broker.broker_name,
-                type='cash'
+                broker=self.broker.broker_name
             ).first()
 
             if strategy_balance is None:
                 strategy_balance = Balance(
                     strategy=self.strategy_name,
                     broker=self.broker.broker_name,
-                    type='cash',
-                    balance=self.starting_capital
+                    balance=self.starting_capital,
+                    type='cash'
                 )
                 session.add(strategy_balance)
                 session.commit()
