5e55640ba6a1454c6d9fa32e5212b139b009e4a1
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:31:53 +0000

    Reset to 637bbe7ab4d8c6c0adce6c1ad2f2a91b94146f8a and made changes for LLM Round 1

--- utils/config.py
+++ utils/config.py
@@ -8,9 +8,9 @@ from strategies.constant_percentage_strategy import ConstantPercentageStrategy
 
 # Mapping of broker types to their constructors
 BROKER_MAP = {
-    'tradier': lambda config, engine: TradierBroker(api_key=config['api_key'], secret_key=None, engine=engine),
-    'etrade': lambda config, engine: EtradeBroker(api_key=config['api_key'], secret_key=config['secret_key'], engine=engine),
-    'tastytrade': lambda config, engine: TastytradeBroker(api_key=config['api_key'], secret_key=config['secret_key'], engine=engine)
+    'tradier': lambda config, engine: TradierBroker(api_key=config['api_key'], secret_key=None, engine=engine, prevent_day_trading=config.get('prevent_day_trading', False)),
+    'etrade': lambda config, engine: EtradeBroker(api_key=config['api_key'], secret_key=config.get('secret_key', None), engine=engine, prevent_day_trading=config.get('prevent_day_trading', False)),
+    'tastytrade': lambda config, engine: TastytradeBroker(api_key=config['api_key'], secret_key=config.get('secret_key', None), engine=engine, prevent_day_trading=config.get('prevent_day_trading', False))
 }
 
 # Mapping of strategy types to their constructors
@@ -49,7 +49,7 @@ def parse_config(config_path):
 
 def initialize_brokers(config):
     # Create a single database engine for all brokers
-    engine = create_engine('sqlite:///default_trading_system.db') if 'database' not in config or 'url' not in config['database'] else create_engine(config['database']['url'])
+    engine = create_engine(config.get('database', {'url': 'sqlite:///default_trading_system.db'})['url']) if 'database' in config else create_engine('sqlite:///default_trading_system.db')
     
     brokers = {}
     for broker_name, broker_config in config['brokers'].items():
