b602865dc6de5a4fb4c6035678a98220ed283ba4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:12:25 +0000

    Reset to 11ccfe4ec5dc4e10465cdca927d5839fc1a1a3d7 and made changes for LLM Round 1

--- ui/app.py
+++ ui/app.py
@@ -1,16 +1,15 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, request
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, func
 from database.models import Trade, AccountInfo, Balance, Position
 from flask_cors import CORS
 import numpy as np
 from scipy.stats import norm
-import os
 
 app = Flask("TradingAPI")
 CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
 
-@app.route('/trades_per_strategy')
+@app.route('/trades_per_strategy', methods=['GET'])
 def trades_per_strategy():
     trades_count = app.session.query(Trade.strategy, Trade.broker, func.count(Trade.id)).group_by(Trade.strategy, Trade.broker).all()
     trades_count_serializable = [{"strategy": strategy, "broker": broker, "count": count} for strategy, broker, count in trades_count]
@@ -23,31 +22,31 @@ def historic_balance_per_strategy():
             Balance.strategy,
             Balance.broker,
             func.strftime('%Y-%m-%d %H', Balance.timestamp).label('hour'),
-            Balance.balance,
+            Balance.total_balance,
         ).group_by(
             Balance.strategy, Balance.broker, 'hour'
         ).order_by(
             Balance.strategy, Balance.broker, 'hour'
         ).all()
         historical_balances_serializable = []
-        for strategy, broker, hour, balance in historical_balances:
+        for strategy, broker, hour, total_balance in historical_balances:
             historical_balances_serializable.append({
                 "strategy": strategy,
                 "broker": broker,
                 "hour": hour,
-                "balance": balance
+                "total_balance": total_balance
             })
         return jsonify({"historic_balance_per_strategy": historical_balances_serializable})
     finally:
         app.session.close()
 
-@app.route('/account_values')
+@app.route('/account_values', methods=['GET'])
 def account_values():
     accounts = app.session.query(AccountInfo).all()
     accounts_data = {account.broker: account.value for account in accounts}
     return jsonify({"account_values": accounts_data})
 
-@app.route('/trade_success_rate')
+@app.route('/trade_success_rate', methods=['GET'])
 def trade_success_rate():
     strategies_and_brokers = app.session.query(Trade.strategy, Trade.broker).distinct().all()
     success_rate_by_strategy_and_broker = []
@@ -67,7 +66,7 @@ def trade_success_rate():
 
     return jsonify({"trade_success_rate": success_rate_by_strategy_and_broker})
 
-@app.route('/positions')
+@app.route('/positions', methods=['GET'])
 def get_positions():
     brokers = request.args.getlist('brokers[]')
     strategies = request.args.getlist('strategies[]')
@@ -118,7 +117,6 @@ def get_trades():
     } for trade in trades]
 
     return jsonify({'trades': trades_data})
-
 
 @app.route('/trade_stats', methods=['GET'])
 def get_trade_stats():
@@ -235,7 +233,6 @@ def get_sharpe_ratio():
     sharpe_ratio = mean_return / std_dev_return if std_dev_return != 0 else 0
 
     return jsonify({'sharpe_ratio': sharpe_ratio})
-
 
 def create_app(engine):
     Session = sessionmaker(bind=engine)
