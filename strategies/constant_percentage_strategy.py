3e9c4e8b557ee16f07902584cd9b49526d47de35
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:12:25 +0000

    Reset to 11ccfe4ec5dc4e10465cdca927d5839fc1a1a3d7 and made changes for LLM Round 1

--- strategies/constant_percentage_strategy.py
+++ strategies/constant_percentage_strategy.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 from strategies.base_strategy import BaseStrategy
-from database.models import Balance
 
 class ConstantPercentageStrategy(BaseStrategy):
     def __init__(self, broker, stock_allocations, cash_percentage, rebalance_interval_minutes, starting_capital):
@@ -15,28 +14,21 @@ class ConstantPercentageStrategy(BaseStrategy):
     def rebalance(self):
         account_info = self.broker.get_account_info()
         cash_balance = account_info.get('cash_available')
-        with self.broker.Session() as session:
-            balance = session.query(Balance).filter_by(
-                strategy=self.strategy_name,
-                broker=self.broker.broker_name,
-                type='cash'
-            ).first()
-            if balance is None:
-                raise ValueError(f"Strategy balance not initialized for {self.strategy_name} strategy on {self.broker}.")
-            total_balance = balance.balance
+        total_balance = account_info.get('total_balance')
+
+        if total_balance is None or cash_balance is None:
+            raise ValueError("Account info is incomplete.")
 
         target_cash_balance = total_balance * self.cash_percentage
         target_investment_balance = total_balance - target_cash_balance
 
         current_positions = self.get_current_positions()
 
-        # TODO: query the number of current positions in the DB for each ticker
-        # associated with this strategy, and then get their current value.
         for stock, allocation in self.stock_allocations.items():
             target_balance = target_investment_balance * allocation
             current_position = current_positions.get(stock, 0)
             current_price = self.broker.get_current_price(stock)
-            target_quantity = target_balance // current_price
+            target_quantity = int(target_balance // current_price)
             if current_position < target_quantity:
                 self.broker.place_order(stock, target_quantity - current_position, 'buy', self.strategy_name)
             elif current_position > target_quantity:
@@ -44,4 +36,4 @@ class ConstantPercentageStrategy(BaseStrategy):
 
     def get_current_positions(self):
         positions = self.broker.get_positions()
-        return {position: positions[position]['quantity'] for position in positions}
+        return {position['symbol']: position['quantity'] for position in positions}
