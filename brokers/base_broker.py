9989b45039e9d253a3e973a095bde70a2885ef7c
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:51:35 +0000

    Reset to ca36e3c7e77879e71d67371fe7716eedee530fa2 and made changes for LLM Round 1

--- brokers/base_broker.py
+++ brokers/base_broker.py
@@ -6,14 +6,14 @@ from database.models import Trade, AccountInfo, Balance, Position
 from datetime import datetime
 
 class BaseBroker(ABC):
-    def __init__(self, api_key, secret_key, broker_name, engine):
+    def __init__(self, api_key, secret_key, broker_name, engine, prevent_day_trading=False):
         self.api_key = api_key
         self.secret_key = secret_key
         self.broker_name = broker_name
         self.db_manager = DBManager(engine)
         self.Session = sessionmaker(bind=engine)
         self.account_id = None
-        self.prevent_day_trading = False  # Initialize prevent_day_trading to False
+        self.prevent_day_trading = prevent_day_trading
 
     @abstractmethod
     def connect(self):
@@ -61,7 +61,7 @@ class BaseBroker(ABC):
             ).all()
             return len(trades) > 0
 
-    def update_positions(self, session, trade, order_type):
+    def update_positions(self, session, trade):
         position = session.query(Position).filter_by(symbol=trade.symbol, broker=self.broker_name, strategy=trade.strategy).first()
 
         if trade.order_type == 'buy':
@@ -125,7 +125,7 @@ class BaseBroker(ABC):
                 session.add(balance)
 
             balance.total_balance += trade.executed_price * trade.quantity
-            self.update_positions(session, trade, order_type)
+            self.update_positions(session, trade)
             session.commit()
 
         return response
