9a08e7c918dcb011e5b7603910a15a1b5a768924
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:12:10 +0000

    Reset to 637bbe7ab4d8c6c0adce6c1ad2f2a91b94146f8a and made changes for LLM Round 1

--- database/models.py
+++ database/models.py
@@ -44,15 +44,11 @@ class Position(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     balance_id = Column(Integer, ForeignKey('balances.id'), nullable=False)
-    strategy = Column(String)
-    broker = Column(String, nullable=False)
     symbol = Column(String, nullable=False)
     quantity = Column(Float, nullable=False)
     latest_price = Column(Float, nullable=False)
-    last_updated = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     balance = relationship("Balance", back_populates="positions")
-
 
 def drop_then_init_db(engine):
     Base.metadata.drop_all(engine)  # Create new tables
