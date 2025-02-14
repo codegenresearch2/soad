06442d40f54b09b5bf471fdc9587e719c0d82ec4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 11:08:21 +0000

    Reset to 4d8c8bf456f1d9e0d860161bee13799fb3aca47a and made changes for LLM Round 1

--- utils/config.py
+++ utils/config.py
@@ -17,7 +17,6 @@ from strategies.simple_strategy import SimpleStrategy
 from .logger import logger
 
 # Mapping of broker types to their constructors
-# TODO: refactor
 BROKER_MAP = {
     'tradier': lambda config, engine: TradierBroker(
         api_key=os.environ.get('TRADIER_API_KEY', config.get('api_key')),
@@ -43,7 +42,6 @@ BROKER_MAP = {
         engine=engine
     )
 }
-
 
 # Mapping of strategy types to their constructors
 STRATEGY_MAP = {
@@ -103,10 +101,9 @@ def load_custom_strategy(broker, strategy_name, config):
         class_name = config['class_name']
         starting_capital = config['starting_capital']
         rebalance_interval_minutes = config['rebalance_interval_minutes']
-        execution_style = config.get('execution_style', '')
         strategy_class = load_strategy_class(file_path, class_name)
         logger.info(f"Initializing custom strategy '{class_name}' with config: {config}")
-        return strategy_class(broker, strategy_name, starting_capital, rebalance_interval_minutes, execution_style, **config.get('strategy_params', {}))
+        return strategy_class(broker, strategy_name, starting_capital, rebalance_interval_minutes, **config.get('strategy_params', {}))
     except Exception as e:
         logger.error(f"Error initializing custom strategy '{config['class_name']}': {e}")
         raise
@@ -175,7 +172,6 @@ def create_api_database_engine(config, local_testing=False):
     if 'database' in config and 'url' in config['database']:
         return create_engine(config['database']['url'])
     return create_engine(os.environ.get("DATABASE_URL", 'sqlite:///default_trading_system.db'))
-
 
 def create_database_engine(config, local_testing=False):
     if local_testing:
